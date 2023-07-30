e897d9cfe19c63a6157740a08831a6f1193f7e9f
iqbalyarkhan <iqbalkhan@Iqbals-MBP.attlocal.net>
2023-07-29 21:49:11 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <h1>My Hacker Stories</h1>
+
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+      <hr />
+      <List />
+
+    </div >
   );
+}
+
+function List() {
+  return (
+    list.map(function (item) {
+      return (
+        <div key={item.objectID}>
+          <h2>Title: <a href={item.url}>{item.title} </a> </h2>
+          <h3>Author: {item.author} </h3>
+          <h4>Num Comments: {item.num_comments}</h4>
+        </div>
+      )
+    })
+  )
 }
 
 export default App;
