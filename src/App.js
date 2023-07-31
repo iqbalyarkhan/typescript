24c245716660984d3383fa29b27f5e22868f69c4
iqbalyarkhan <iqbalkhan@Iqbals-MBP.attlocal.net>
2023-07-30 21:27:13 -0500

    more react

--- src/App.js
+++ src/App.js
@@ -1,42 +1,72 @@
 import React from 'react';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
 function App() {
+
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+    {
+      title: 'Designing Data Intensive Apps',
+      url: 'https://redux.js.org/',
+      author: 'Me',
+      num_comments: 4,
+      points: 7,
+      objectID: 2,
+    },
+  ];
+
+  const [searchTerm, setSearchTerm] = React.useState('');
+
+  const handleSearch = event => {
+    setSearchTerm(event.target.value);
+  };
+
+  const searchedStories = stories.filter(story => {
+    return story.title
+      .toLowerCase()
+      .includes(searchTerm.toLowerCase());
+  });
 
   return (
     <div>
       <h1>My Hacker Stories</h1>
-
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
-      <hr />
-      <List />
+      <Search onSearch={handleSearch} searchTerm={searchTerm} />
+      <List list={searchedStories} />
 
     </div >
   );
 }
 
-function List() {
+const Search = props => {
+
   return (
-    list.map(function (item) {
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={props.onSearch} />
+      <p>
+        Searching for <strong>{props.searchTerm}</strong>.
+      </p>
+    </div>
+  );
+};
+
+const List = props => {
+  return (
+    props.list.map(function (item) {
       return (
         <div key={item.objectID}>
           <h2>Title: <a href={item.url}>{item.title} </a> </h2>
